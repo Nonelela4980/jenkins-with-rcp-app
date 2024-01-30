f363757f20e19deb85257b4471ddbfb04d5339e0
Nonelela Cele <nonelela.cele@s4.co.za>
2024-01-30 10:09:19 +0200

    scripts folder

---
+++ scrips/script.sh
@@ -0,0 +1,10 @@
+echo "===========================STARTED MAVEN========================"
+echo "cleaning......."
+
+ls
+
+cd ../parent
+
+mvn clean
+
+echo "===========================MAVEN CLEAN COMPLETED========================"
